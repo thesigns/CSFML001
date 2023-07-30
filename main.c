ff9b2365814b8386da2cc515296b82a061adb2e6
Jakub <92330684+thesigns@users.noreply.github.com>
2023-07-30 23:00:03 +0200

    Learning CSFML

--- main.c
+++ main.c
@@ -1,8 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <SFML\System.h>
+#include <SFML\Window.h>
+#include <SFML\Graphics.h>
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+
+    sfVideoMode desktopMode = sfVideoMode_getDesktopMode();
+    sfContextSettings contextSettings = {0, 0, 8, 1, 1, 0, false};
+
+    sfRenderWindow *window = sfRenderWindow_create(desktopMode, "CSFML001 Fun", sfDefaultStyle, &contextSettings);
+    if (!window)
+    {
+        return EXIT_FAILURE;
+    }
+
+    while (sfRenderWindow_isOpen(window))
+    {
+
+        sfEvent event;
+        while (sfRenderWindow_pollEvent(window, &event))
+        {
+            if (event.type == sfEvtClosed)
+            {
+                sfRenderWindow_close(window);
+            }
+            if (event.type == sfEvtKeyPressed)
+            {
+                if (event.key.code == sfKeyEscape)
+                {
+                    sfRenderWindow_close(window);
+                }
+                printf("%i ", event.key.code);
+            }
+        }
+        sfRenderWindow_clear(window, sfBlack);
+        sfRenderWindow_display(window);
+    }
+
+    sfRenderWindow_destroy(window);
+
+    return EXIT_SUCCESS;
 }
